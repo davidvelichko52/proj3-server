779c076eb41256d866c8210f55a7d75bcd0cc16e
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-05-19 15:17:30 -0700

    created routes for posts and for profile

--- controllers/faves.js
+++
@@ -1,6 +0,0 @@
-
-//TODO get routes for seeing all your favorites
-
-//TODO post route for sending clicked faves to user faves list
-
-//TODO make it so when clicked again we un favorite probably put route  
