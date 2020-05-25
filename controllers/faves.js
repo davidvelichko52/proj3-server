1b548960567d223218ed1b9599e9ebc2a7439e31
davidvelichko52 <davidvelichko52@gmail.com>
2020-05-25 14:28:26 -0700

    trying to make it work

--- controllers/faves.js
+++ controllers/faves.js
@@ -1,8 +1,6 @@
-
 const router = require('express').Router()
 const db = require('../models')
 router.use(require('express').static('static'))
-
 // fetch call
 router.post('/', (req, res) => {
     console.log('YOOOOOOOOO!!!', req.body)
@@ -17,7 +15,6 @@ router.post('/', (req, res) => {
         res.send('Uh oh!')
     })
 })
-
 router.get('/:id', (req, res) => {
     db.Fave.find({
         userId: req.params.id
@@ -32,7 +29,6 @@ router.get('/:id', (req, res) => {
         res.send('Uh oh!')
     })
 })
-
 router.get('/', (req, res) => {
     db.Fave.find()
     // .populate('postId')
@@ -44,11 +40,9 @@ router.get('/', (req, res) => {
         res.send('Uh oh!')
     })
 })
-
 router.get('/user/:userId', async (req, res) => { 
     // this is the ID of the user whose favorite posts we want to show
     let myUserId = req.params.userId;
-
     // step 1 summary: this gets the user's favorites (NOT POSTS) --> SECTION A
     // These favorites contain POST IDs, which we need to get the actual POST OBJECTS
     let myPromise = () => { 
@@ -62,15 +56,12 @@ router.get('/user/:userId', async (req, res) => {
             })
         })
     }
-
     // step 2 summary: now that we have the FAVE OBJECTS, we can get the related POST IDs.
     // using these POST IDs, we can get each POST OBJECT (SECTION B)
     await(myPromise()).then((faves) => {
-        
         // for each of our FAVE OBJECTS, we get the POST ID and get the POST OBJECT from the database
         let promises = faves.map((singleFave) => {
             postId = singleFave.postId;
-            
             // (these are the INDIVIDUAL TASKS in the middle of the diagram (SECTION B)
             // these tasks aren't actually performed until later below..)
             // this will get the POST OBJECT for each POST ID
@@ -83,17 +74,14 @@ router.get('/user/:userId', async (req, res) => {
                 return jsonPost;
             })
         })
-        
         // step 3 summary: this final step PERFORMS ALL THE STEPS IN SECTION B, and combines the results.
         // the result is SECTION C: the posts that are sent back to the client
         Promise.all(promises).then((posts) => {
             // console.log('postsss', posts);
-
             res.json(posts); // send POST OBJECTS back to client
         })
     })
 })
-
 router.delete('/:id', (req, res) => {
     console.log('DELETING');
     console.log('Yo', req.params.id)
@@ -107,6 +95,5 @@ router.delete('/:id', (req, res) => {
         console.log("error in Delete single post route", err)
     })
 })
-
 module.exports = router
 
